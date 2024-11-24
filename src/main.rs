80240ddf56e1995e82effbf4ef2f94ec15f2687a
Mark Devenyi <markdevenyidev@gmail.com>
2024-11-23 19:46:17 -0500

    feat(backend): add support for serving Svelte frontend

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,29 @@
-use axum::{routing::get, Router};
+use axum::Router;
+
+const SERVER_PORT: &str = "3000";
+const SERVER_HOST: &str = "0.0.0.0";
+
+const FRONTEND: &str = "public";
+
+mod routes;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let app = Router::new().merge(routes::svelte(FRONTEND));
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    axum::serve(listener, app).await.unwrap();
+    let listener = tokio::net::TcpListener::bind(format!("{SERVER_HOST}:{SERVER_PORT}"))
+        .await
+        .unwrap();
+
+    axum::serve(listener, app)
+        .with_graceful_shutdown(shutdown_signal())
+        .await
+        .unwrap();
+}
+
+async fn shutdown_signal() {
+    tokio::signal::ctrl_c()
+        .await
+        .expect("Expect shutdown signal handler");
+    println!("[offline] fsh");
 }
