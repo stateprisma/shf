f56556efc6eadc6d0b907fe076d887752fc6913f
Wrench56 <markdevenyidev@gmail.com>
2024-11-26 18:52:55 -0500

    feat(frontend): query specified folder on startup

---
+++ frontend/src/lib/types/communication.types.ts
@@ -0,0 +1,4 @@
+export enum Types {
+	Error = 0,
+	Query = 1
+}
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,10 +1,22 @@
 <script lang="ts">
 	import Header from '$lib/Header.svelte';
+	import { encodeMsg } from '$lib/protocol';
 	import { socketManager } from '$lib/socket';
+	import { Types } from '$lib/types/communication.types';
+
 	import { onMount } from 'svelte';
 
 	onMount(() => {
 		socketManager.connect(`ws://${window.location.host}/connect`);
+		socketManager.subscribeConnection((con: boolean) => {
+			if (!con) return;
+			socketManager.send(
+				encodeMsg({
+					type: Types.Query,
+					args: { path: window.location.search.replace('?query=', '') || '/' }
+				})
+			);
+		});
 	});
 </script>
 
