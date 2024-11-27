be6bf1df05854b4c978272b1750eb34ad425dc6a
Mark Devenyi <markdevenyidev@gmail.com>
2024-11-27 18:42:44 -0500

    feat(backend): implement `Query` message handling

--- src/messages/types.rs
+++ src/messages/types.rs
@@ -1,15 +1,18 @@
 use std::vec;
 
-use axum::extract::ws::WebSocket;
-use futures_util::stream::SplitSink;
-use rmp_serde::decode;
+use axum::extract::ws::{Message, WebSocket};
+use futures_util::{stream::SplitSink, SinkExt};
+use rmp_serde::{decode, encode};
 use serde::{Deserialize, Serialize};
+
+use crate::filesystem::query::{list_directory, FileEntry};
 
 pub type ConcurrentWebSocket = SplitSink<WebSocket, axum::extract::ws::Message>;
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, Serialize)]
 pub enum Types {
     Error = 0,
+    Query = 1,
 }
 
 pub trait FromMsgPack<T: Default + for<'a> Deserialize<'a>> {
@@ -26,7 +29,7 @@ pub trait FromMsgPack<T: Default + for<'a> Deserialize<'a>> {
 
 pub trait ProcessMsg<T: Default + for<'a> Deserialize<'a>> {
     /// Process incoming message
-    async fn process(&self, websocket: &ConcurrentWebSocket);
+    async fn process(&self, websocket: &mut ConcurrentWebSocket);
 }
 
 #[derive(Deserialize, Debug)]
@@ -49,13 +52,70 @@ impl Default for CommonMsg {
 impl FromMsgPack<CommonMsg> for CommonMsg {}
 
 impl ProcessMsg<CommonMsg> for CommonMsg {
-    async fn process(&self, _: &ConcurrentWebSocket) {
+    async fn process(&self, socket: &mut ConcurrentWebSocket) {
         println!("[type] {:?}", self.t);
         match self.t {
             Types::Error => (),
+            Types::Query => QueryMsg::from_msgpack(&self.a).process(socket).await,
         }
     }
 }
 
 #[derive(Serialize)]
-pub struct QueryResp {}
+struct CommonResp {
+    /* Type */
+    t: Types,
+    /* Arguments */
+    a: Vec<u8>,
+}
+
+#[derive(Deserialize)]
+pub struct QueryMsg {
+    path: String,
+}
+
+impl Default for QueryMsg {
+    fn default() -> Self {
+        Self {
+            path: Default::default(),
+        }
+    }
+}
+
+impl FromMsgPack<QueryMsg> for QueryMsg {}
+impl QueryMsg {
+    pub async fn process(&self, socket: &mut ConcurrentWebSocket) {
+        if let Ok(entries) = list_directory(&self.path) {
+            let resp = CommonResp {
+                t: Types::Query,
+                a: match encode::to_vec(&QueryResp { entries }) {
+                    Ok(data) => data,
+                    Err(e) => {
+                        eprintln!("[err ] Failed to encode QueryResp: {:?}", e);
+                        return;
+                    }
+                },
+            };
+
+            if let Err(e) = socket
+                .send(Message::Binary(match encode::to_vec(&resp) {
+                    Ok(msg) => msg,
+                    Err(e) => {
+                        eprintln!("[err ] Failed to encode CommonResp: {:?}", e);
+                        return;
+                    }
+                }))
+                .await
+            {
+                eprintln!("[err ] Error sending Query response: {:?}", e);
+            }
+        } else {
+            eprintln!("[err ] Failed to list directory");
+        }
+    }
+}
+
+#[derive(Serialize)]
+pub struct QueryResp {
+    entries: Vec<FileEntry>,
+}
