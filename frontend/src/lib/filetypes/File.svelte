7748363322352c0a213797226201eda3bb7ddd0f
Mark Devenyi <markdevenyidev@gmail.com>
2024-11-27 20:07:40 -0500

    feat(frontend): process `Query` messages and display entries in `FileListView`

---
+++ frontend/src/lib/filetypes/File.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+	import type { FileEntry } from '$lib/types/query.types';
+
+	export let descriptor: FileEntry;
+</script>
+
+<div class="file-entry file">
+	<div class="icon">📄</div>
+	<div class="name">{descriptor.name}</div>
+	{#if descriptor.size}
+		<div class="size">{descriptor.size} bytes</div>
+	{/if}
+</div>
+
+<style>
+	.file-entry {
+		display: flex;
+		align-items: center;
+		padding: 10px;
+		margin: 5px;
+		background-color: #f5f5f5;
+		border-radius: 5px;
+		width: 100%;
+		box-sizing: border-box;
+		cursor: pointer;
+		transition: background-color 0.3s;
+	}
+
+	.file-entry:hover {
+		background-color: #e2e2e2;
+	}
+
+	.icon {
+		font-size: 24px;
+		margin-right: 10px;
+	}
+
+	.name {
+		font-size: 16px;
+		font-weight: bold;
+		overflow: hidden;
+		white-space: nowrap;
+		text-overflow: ellipsis;
+	}
+
+	.size {
+		font-size: 12px;
+		margin-left: auto;
+		color: #777;
+	}
+
+	.file {
+		background-color: #d4f4dd;
+	}
+</style>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,10 +1,14 @@
 <script lang="ts">
 	import Header from '$lib/Header.svelte';
-	import { encodeMsg } from '$lib/protocol';
+	import ListView from '$lib/FileListView.svelte';
+	import { decodeMsg, encodeMsg } from '$lib/protocol';
 	import { socketManager } from '$lib/socket';
 	import { Types } from '$lib/types/communication.types';
 
 	import { onMount } from 'svelte';
+	import type { FileEntry } from '$lib/types/query.types';
+
+	let entries: Array<FileEntry> = [];
 
 	onMount(() => {
 		socketManager.connect(`ws://${window.location.host}/connect`);
@@ -17,7 +21,22 @@
 				})
 			);
 		});
+
+		socketManager.subscribeMessages((msg: Uint8Array) => {
+			let [type, resp] = decodeMsg(msg);
+			if (resp === null) {
+				console.error('Error while decoding incoming response');
+				return;
+			}
+
+			switch (type) {
+				case Types.Query:
+					console.log(resp.entries);
+					entries = resp.entries;
+			}
+		});
 	});
 </script>
 
 <Header />
+<ListView {entries} />
