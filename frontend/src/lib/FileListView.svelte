5cff22be17ccb34c7ec2679536d0c9f861c8b470
Wrench56 <markdevenyidev@gmail.com>
2024-11-27 20:07:40 -0500

    feat(frontend): process `Query` messages and display entries in `FileListView`

---
+++ frontend/src/lib/FileListView.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import Directory from './filetypes/Directory.svelte';
+	import File from './filetypes/File.svelte';
+	import Symlink from './filetypes/Symlink.svelte';
+	import { FileEntryType, type FileEntry } from './types/query.types';
+
+	export let entries: Array<FileEntry>;
+</script>
+
+{#each entries as entry}
+	{#if entry.type_ === FileEntryType.File}
+		<File descriptor={entry} />
+	{:else if entry.type_ === FileEntryType.Directory}
+		<Directory descriptor={entry} />
+	{:else if entry.type_ === FileEntryType.Symlink}
+		<Symlink descriptor={entry} />
+	{:else if entry.type_ === FileEntryType.Unknown}
+		<h1>ERROR: UNKNOWN</h1>
+	{/if}
+{/each}
---
+++ frontend/src/lib/filetypes/Symlink.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+	import type { FileEntry } from '$lib/types/query.types';
+
+	export let descriptor: FileEntry;
+</script>
+
+<div class="file-entry symlink">
+	<div class="icon">🔗</div>
+	<div class="name">{descriptor.name}</div>
+</div>
+
+<style>
+	.file-entry {
+		display: flex;
+		align-items: center;
+		padding: 10px;
+		margin: 5px;
+		background-color: #f5f5f5;
+		border-radius: 5px;
+		width: 100%;
+		box-sizing: border-box;
+		cursor: pointer;
+		transition: background-color 0.3s;
+	}
+
+	.file-entry:hover {
+		background-color: #e2e2e2;
+	}
+
+	.icon {
+		font-size: 24px;
+		margin-right: 10px;
+	}
+
+	.name {
+		font-size: 16px;
+		font-weight: bold;
+		overflow: hidden;
+		white-space: nowrap;
+		text-overflow: ellipsis;
+	}
+
+	.symlink {
+		background-color: #f0f0f0;
+	}
+</style>
