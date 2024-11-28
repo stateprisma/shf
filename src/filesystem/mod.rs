a909550ae94872e5a9f5aa46fb7fb58536e383fc
Wrench56 <markdevenyidev@gmail.com>
2024-11-27 23:14:48 -0500

    refactor(backend): move `escape_path()` in `filesystem/mod.rs` for reuse

--- src/filesystem/mod.rs
+++ src/filesystem/mod.rs
@@ -9,3 +9,14 @@ lazy_static! {
     pub static ref WORKING_DIR: PathBuf =
         std::env::current_dir().expect("Failed to get working directory");
 }
+
+pub fn escape_path(relative_path: &str) -> PathBuf {
+    if relative_path.starts_with('/') {
+        WORKING_DIR.join(&relative_path[1..])
+    } else if relative_path.contains("..") {
+        println!("[warn] Attempted use of \"..\" blocked");
+        escape_path(&relative_path.replace("../", "").replace("..", ""))
+    } else {
+        WORKING_DIR.join(relative_path)
+    }
+}
