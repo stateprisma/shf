e19bbf9f33ad95ab370a3456f1ef430ee3d46f24
Mark Devenyi <markdevenyidev@gmail.com>
2024-11-27 21:51:26 -0500

    feat(frontend): add functionality to go up one directory level

--- frontend/src/lib/FileListView.svelte
+++ frontend/src/lib/FileListView.svelte
@@ -4,10 +4,28 @@
 	import Symlink from './filetypes/Symlink.svelte';
 	import { FileEntryType, type FileEntry } from './types/query.types';
 
-	export let entries: Array<FileEntry>;
+	let adjustedEntries: FileEntry[] = $state([]);
+	let { entries } = $props();
+
+	function getQueryParam(): string {
+		if (typeof window !== 'undefined') {
+			return window.location.search.replace('?query=', '') || '/';
+		} else {
+			return '/';
+		}
+	}
+
+	$effect(() => {
+		adjustedEntries = [
+			...(getQueryParam() !== '/'
+				? [{ name: '..', size: 0, type_: FileEntryType.Directory, last_modified: 0 }]
+				: []),
+			...entries
+		];
+	});
 </script>
 
-{#each entries as entry}
+{#each adjustedEntries as entry}
 	{#if entry.type_ === FileEntryType.File}
 		<File descriptor={entry} />
 	{:else if entry.type_ === FileEntryType.Directory}
