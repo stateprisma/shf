3fd360ae9ae4593146fbe1d8d3b3e15f931c1b78
Mark Devenyi <markdevenyidev@gmail.com>
2024-11-27 20:02:20 -0500

    feat(frontend): add `decodeMsg()` to `protocol.ts`

--- frontend/src/lib/protocol.ts
+++ frontend/src/lib/protocol.ts
@@ -1,6 +1,19 @@
-import { encode } from '@msgpack/msgpack';
-import type { Types } from './types/communication.types';
+import { decode, encode } from '@msgpack/msgpack';
+import { Types, type CommonResp, type Responses } from './types/communication.types';
+import type { QueryResp } from './types/query.types';
 
 export function encodeMsg(params: { type: Types; args: object }): Uint8Array {
 	return encode({ t: params.type, a: encode(params.args) });
+}
+
+export function decodeMsg(msg: Uint8Array): [Types | null, Responses | null] {
+	const cresp: CommonResp = decode(msg) as CommonResp;
+	switch (cresp.t) {
+		case Types.Error:
+			return [Types.Error, null];
+		case Types.Query:
+			return [Types.Query, decode(cresp.a) as QueryResp];
+	}
+
+	return [null, null];
 }
